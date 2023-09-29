3fc7b139926d70f1086543b00f55732629744fbd
vinit <vinitsharma2480@gmail.com>
2023-09-29 20:53:58 +0530

    Fixing entry.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { registerRootComponent } from 'expo';
+import Index from './Index';
+
+function App() {
+    return <Index />;
+}
+
+registerRootComponent(App);
+
+
+export default App
